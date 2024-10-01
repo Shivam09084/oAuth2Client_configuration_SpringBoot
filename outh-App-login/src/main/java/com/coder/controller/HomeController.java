dbd52f6aefdde735dc4792dbe33e44dbaffe091a
Shivam <Shivam09084@gmail.com>
2024-10-02 00:56:57 +0530

    homePage1 method

--- outh-App-login/src/main/java/com/coder/controller/HomeController.java
+++ outh-App-login/src/main/java/com/coder/controller/HomeController.java
@@ -4,12 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class HomeController {
 	
 	@GetMapping("/")
 	public ResponseEntity<?> homPage(){
+		
+		return new ResponseEntity<>("I'm A Java Developer",HttpStatus.OK);
+	}
+	@GetMapping("/abc")
+	
+	public ResponseEntity<?> homPage1(){
 		
 		return new ResponseEntity<>("I'm A Java Developer",HttpStatus.OK);
 	}
